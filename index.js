7634f9d9da7a98899d83971ec98388208ff4eadc
Theo-Sempaii <emanuel250gameryt@gmail.com>
2021-08-16 17:31:56 -0300

    microfunctions

---
+++ index.js
@@ -0,0 +1,6 @@
+const createImage = require('./src/utils/generator/createImage')
+const setStyle = require('./src/utils/generator/setStyle')
+module.exports = {
+  createImage,
+  setStyle
+}
---
+++ src/utils/generator/setStyle.js
@@ -0,0 +1,17 @@
+const { CanvasRenderingContext2D, loadImage } = require('canvas')
+
+/**
+ * 
+ * @param {CanvasRenderingContext2D} ctx The canvas base to create a logotype!
+ * @param {String} text The text you want to make logo!
+ * @param {[String]} textColors The colors that the text will have, is an array of 3 colors to give depth!
+ * @param {String} backgroundStyle The style of the background to generate!
+ * @param {[String]} backgroundColors The 3 colors in the background!
+ * @returns 
+ */
+
+async function setStyle(ctx, text, textColors, backgroundStyle, backgroundColors) {
+  
+}
+
+module.exports = setStyle;
